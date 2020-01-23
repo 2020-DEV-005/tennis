a6ba76386fcc9ec9299178e98b84d9c39ebc93ac
Chandrashekar Reddy Muthumul <782902@TCSNSN.COM>
2020-01-23 12:30:04 +0530

    Player headings added to the Game component

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {AppConst} from '../../constants/App.const';
+import './Game.css';
 
 class Game extends React.Component {
     render = () => {
-        return null;
+        return (
+            <div className="game">
+                <div className="player">
+                    <h4>{AppConst.PLAYER_1}</h4>
+                </div>
+                <div className="player">
+                    <h4>{AppConst.PLAYER_2}</h4>
+                </div>
+            </div>
+        );
     }
 }
 
